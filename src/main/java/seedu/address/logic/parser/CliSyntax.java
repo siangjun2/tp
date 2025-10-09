6373c80aa0d5a08de1b928dc23b6ed4d3aa89d22
zichen05 <e1481842@u.nus.edu>
2025-10-10 00:16:17 +0800

    Fix the bug within PaymentCommand

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -13,5 +13,6 @@ public class CliSyntax {
     public static final Prefix PREFIX_ROLE = new Prefix("r/");
     public static final Prefix PREFIX_CLASS = new Prefix("c/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
+    public static final Prefix PREFIX_STATUS = new Prefix("s/");
 
 }
